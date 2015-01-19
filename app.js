e7cb78f3c0c145bf6834238797af0c56437fca2d
Ming He <meticulous.dft@gmail.com>
2015-01-19 00:43:59 -0800

    add cors support

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ app.use(express.json());
 app.use(express.urlencoded());
 app.use(methodOverride());
 
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  // intercept OPTIONS method
+  if (req.method == 'OPTIONS') {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+});
+
 require('./routes')(app); // load our routes and pass in our app
 
 app.set('port', process.env.PORT || 3005);
